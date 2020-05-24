f59a68bd342e90ccb8e43cca47b79f815644159b
Stanoque <stanoquedzhumpsh@gmail.com>
2020-05-25 00:32:39 +0300

    Test.

--- Table/src/javascript/const_serverResponseTime.js
+++
@@ -1,3 +0,0 @@
-
-// This file exports value of the server response time in ms.
-module.exports = 2000;
--- Table/src/javascript/price_vidget/cleanPriceString.js
+++
@@ -1,5 +0,0 @@
-// This simple function just cleans price string from semis and dollars
-
-module.exports = function(price){
-  return price.replace(/\,/g, '').replace('$', '');
-};
